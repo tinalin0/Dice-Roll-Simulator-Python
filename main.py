93b7022715cebe39dabbaad91b5356933926aa8c
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2023-01-12 14:45:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # Dice Roll Simulator
+loop = True
 
-# Display Menu
-print("Dice Roll Simulator Menu \n1. Roll Dice Once \n2. Roll Dice 5 Times \n3. Roll Dice 'n' Times \n4. Roll Dice until Snake Eyes \n5. Exit")
-# Input
-option = input("Select an option (1-5): ")
+# Loop until option is 5
+while loop == True:
+    # Display Menu
+    print("Dice Roll Simulator Menu")
+    print("1. Roll Dice Once")
+    print("2. Roll Dice 5 Times")
+    print("3. Roll Dice 'n' Times")
+    print("4. Roll Dice until Snake Eyes")
+    print("5. Exit")
+    # Input
+    option = input("Select an option (1-5): ")
