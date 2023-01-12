cf4aed88277554b8b0460c6c11e251d6d255ff74
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2023-01-12 14:38:02 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Dice Roll Simulator
+
+# Display Menu
+print("Dice Roll Simulator Menu \n1. Roll Dice Once \n2. Roll Dice 5 Times \n3. Roll Dice 'n' Times \n4. Roll Dice until Snake Eyes \n5. Exit")
+# Input
+option = input("Select an option (1-5): ")
